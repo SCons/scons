16a3b3c891ae3a04dfe50c2c6473b781c8194a15
Steven Knight <knight@baldmt.com>
2001-09-20 16:08:01 +0000

    Cygwin portability fixes for Windows NT builds.

--- src/script/setup.py
+++ src/script/setup.py
@@ -1,6 +1,14 @@
 __revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
-from string import join, split
+import os
+import os.path
+import sys
+
+(head, tail) = os.path.split(sys.argv[0])
+
+if head:
+    os.chdir(head)
+    sys.argv[0] = tail
 
 from distutils.core import setup
 
