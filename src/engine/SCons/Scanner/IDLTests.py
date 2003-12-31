0a09be081d754e184e61f8815933ac5a4a43697f
Steven Knight <knight@baldmt.com>
2003-12-31 16:17:50 +0000

    Fix __COPYRIGHT__ and __REVISION__ lines; make IDLTests.py as an Aegis test, not a source file.

--- src/engine/SCons/Scanner/IDLTests.py
+++ src/engine/SCons/Scanner/IDLTests.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2001, 2002, 2003 Steven Knight
+# __COPYRIGHT__
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -21,7 +21,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-__revision__ = "__REVISION__"
+__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 import TestCmd
 import SCons.Scanner.IDL
