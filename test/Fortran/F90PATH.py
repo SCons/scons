89330e2ad1be10e91444afc5b3979a23d1b853fe
Steven Knight <knight@baldmt.com>
2004-07-29 13:28:56 +0000

    Add Fortran 90/95 support.  (Chris Murray)

---
+++ test/Fortran/F90PATH.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+#
+# __COPYRIGHT__
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
+
+import os
+import os.path
+import sys
+import TestSCons
+
+_exe = TestSCons._exe
+FTN_LIB = TestSCons.fortran_lib
+prog = 'prog' + _exe
+subdir_prog = os.path.join('subdir', 'prog' + _exe)
+variant_prog = os.path.join('variant', 'prog' + _exe)
+
+args = prog + ' ' + subdir_prog + ' ' + variant_prog
+
+test = TestSCons.TestSCons()
+
+#if not test.detect('F90', 'g90'):
+#    test.pass_test()
+base = '/opt/intel_fc_80'
+F90 = os.path.join(base, 'bin', 'ifort')
+LIBPATH = os.path.join(base, 'lib')
+LIBS = ['irc']
+if not os.path.exists(F90):
+   sys.stderr.write('No (hard-coded) F90 compiler %s\n' % F90)
+   test.no_result(1)
+
+os.environ['LD_LIBRARY_PATH'] = LIBPATH
+    
+test.subdir('include', 'subdir', ['subdir', 'include'], 'inc2')
+
+test.write('SConstruct', """
+env = Environment(F90 = r'%s',
+                  F90PATH = ['$FOO'],
+                  LINK = '$F90',
+                  LIBPATH = %s,
+                  LIBS = %s,
+                  FOO='include')
+obj = env.Object(target='foobar/prog', source='subdir/prog.f90')
+env.Program(target='prog', source=obj)
+SConscript('subdir/SConscript', "env")
+
+BuildDir('variant', 'subdir', 0)
+include = Dir('include')
+env = Environment(F90 = r'%s',
+                  F90PATH=[include],
+                  LINK = '$F90',
+                  LIBPATH = %s,
+                  LIBS = %s)
+SConscript('variant/SConscript', "env")
+""" % (F90, repr(LIBPATH), LIBS, F90, repr(LIBPATH), LIBS))
+
+test.write(['subdir', 'SConscript'],
+"""
+Import("env")
+env.Program(target='prog', source='prog.f90')
+""")
+
+test.write(['include', 'foo.f90'],
+r"""
+      PRINT *, 'include/foo.f90 1'
+      INCLUDE 'bar.f90'
+""")
+
+test.write(['include', 'bar.f90'],
+r"""
+      PRINT *, 'include/bar.f90 1'
+""")
+
+test.write(['subdir', 'prog.f90'],
+r"""
+      PROGRAM PROG
+      PRINT *, 'subdir/prog.f90'
+      include 'foo.f90'
+      STOP
+      END
+""")
+
+test.write(['subdir', 'include', 'foo.f90'],
+r"""
+      PRINT *, 'subdir/include/foo.f90 1'
+      INCLUDE 'bar.f90'
+""")
+
+test.write(['subdir', 'include', 'bar.f90'],
+r"""
+      PRINT *, 'subdir/include/bar.f90 1'
+""")
+
+
+
+test.run(arguments = args)
+
+test.run(program = test.workpath(prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 1\n include/bar.f90 1\n")
+
+test.run(program = test.workpath(subdir_prog),
+         stdout = " subdir/prog.f90\n subdir/include/foo.f90 1\n subdir/include/bar.f90 1\n")
+
+test.run(program = test.workpath(variant_prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 1\n include/bar.f90 1\n")
+
+# Make sure we didn't duplicate the source file in the variant subdirectory.
+test.must_not_exist(test.workpath('variant', 'prog.f90'))
+
+test.up_to_date(arguments = args)
+
+test.write(['include', 'foo.f90'],
+r"""
+      PRINT *, 'include/foo.f90 2'
+      INCLUDE 'bar.f90'
+""")
+
+test.run(arguments = args)
+
+test.run(program = test.workpath(prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 2\n include/bar.f90 1\n")
+
+test.run(program = test.workpath(subdir_prog),
+         stdout = " subdir/prog.f90\n subdir/include/foo.f90 1\n subdir/include/bar.f90 1\n")
+
+test.run(program = test.workpath(variant_prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 2\n include/bar.f90 1\n")
+
+# Make sure we didn't duplicate the source file in the variant subdirectory.
+test.must_not_exist(test.workpath('variant', 'prog.f90'))
+
+test.up_to_date(arguments = args)
+
+#
+test.write(['include', 'bar.f90'],
+r"""
+      PRINT *, 'include/bar.f90 2'
+""")
+
+test.run(arguments = args)
+
+test.run(program = test.workpath(prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 2\n include/bar.f90 2\n")
+
+test.run(program = test.workpath(subdir_prog),
+         stdout = " subdir/prog.f90\n subdir/include/foo.f90 1\n subdir/include/bar.f90 1\n")
+
+test.run(program = test.workpath(variant_prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 2\n include/bar.f90 2\n")
+
+# Make sure we didn't duplicate the source file in the variant subdirectory.
+test.must_not_exist(test.workpath('variant', 'prog.f90'))
+
+test.up_to_date(arguments = args)
+
+# Change F90PATH and make sure we don't rebuild because of it.
+test.write('SConstruct', """
+env = Environment(F90 = r'%s',
+                  F90PATH = Split('inc2 include'),
+                  LINK = '$F90',
+                  LIBPATH = %s,
+                  LIBS = %s)
+obj = env.Object(target='foobar/prog', source='subdir/prog.f90')
+env.Program(target='prog', source=obj)
+SConscript('subdir/SConscript', "env")
+
+BuildDir('variant', 'subdir', 0)
+include = Dir('include')
+env = Environment(F90 = r'%s',
+                  F90PATH=['inc2', include],
+                  LINK = '$F90',
+                  LIBPATH = %s,
+                  LIBS = %s)
+SConscript('variant/SConscript', "env")
+""" % (F90, repr(LIBPATH), LIBS, F90, repr(LIBPATH), LIBS))
+
+test.up_to_date(arguments = args)
+
+#
+test.write(['inc2', 'foo.f90'],
+r"""
+      PRINT *, 'inc2/foo.f90 1'
+      INCLUDE 'bar.f90'
+""")
+
+test.run(arguments = args)
+
+test.run(program = test.workpath(prog),
+         stdout = " subdir/prog.f90\n inc2/foo.f90 1\n include/bar.f90 2\n")
+
+test.run(program = test.workpath(subdir_prog),
+         stdout = " subdir/prog.f90\n subdir/include/foo.f90 1\n subdir/include/bar.f90 1\n")
+
+test.run(program = test.workpath(variant_prog),
+         stdout = " subdir/prog.f90\n include/foo.f90 2\n include/bar.f90 2\n")
+
+test.up_to_date(arguments = args)
+
+test.pass_test()
---
+++ test/Fortran/SHF77FLAGS.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+#
+# __COPYRIGHT__
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
+
+import os
+import string
+import sys
+import TestSCons
+
+python = TestSCons.python
+
+if sys.platform == 'win32':
+    _obj = '.obj'
+else:
+    if string.find(sys.platform, 'irix') > -1:
+        _obj = '.o'
+    else:
+        _obj = '.os'
+
+test = TestSCons.TestSCons()
+
+
+
+test.write('myg77.py', r"""
+import getopt
+import sys
+opts, args = getopt.getopt(sys.argv[1:], 'co:x')
+optstring = ''
+for opt, arg in opts:
+    if opt == '-o': out = arg
+    else: optstring = optstring + ' ' + opt
+infile = open(args[0], 'rb')
+outfile = open(out, 'wb')
+outfile.write(optstring + "\n")
+for l in infile.readlines():
+    if l[:4] != '#g77':
+	outfile.write(l)
+sys.exit(0)
+""")
+
+
+
+test.write('SConstruct', """
+env = Environment(SHF77 = r'%s myg77.py',
+                  SHF77FLAGS = '-x')
+env.SharedObject(target = 'test01', source = 'test01.f')
+env.SharedObject(target = 'test02', source = 'test02.F')
+env.SharedObject(target = 'test03', source = 'test03.for')
+env.SharedObject(target = 'test04', source = 'test04.FOR')
+env.SharedObject(target = 'test05', source = 'test05.ftn')
+env.SharedObject(target = 'test06', source = 'test06.FTN')
+env.SharedObject(target = 'test07', source = 'test07.fpp')
+env.SharedObject(target = 'test08', source = 'test08.FPP')
+env.SharedObject(target = 'test09', source = 'test09.f77')
+env.SharedObject(target = 'test10', source = 'test10.F77')
+""" % (python,))
+
+test.write('test01.f',   "This is a .f file.\n#g77\n")
+test.write('test02.F',   "This is a .F file.\n#g77\n")
+test.write('test03.for', "This is a .for file.\n#g77\n")
+test.write('test04.FOR', "This is a .FOR file.\n#g77\n")
+test.write('test05.ftn', "This is a .ftn file.\n#g77\n")
+test.write('test06.FTN', "This is a .FTN file.\n#g77\n")
+test.write('test07.fpp', "This is a .fpp file.\n#g77\n")
+test.write('test08.FPP', "This is a .FPP file.\n#g77\n")
+test.write('test09.f77', "This is a .f77 file.\n#g77\n")
+test.write('test10.F77', "This is a .F77 file.\n#g77\n")
+
+test.run(arguments = '.', stderr = None)
+
+test.must_match('test01' + _obj, " -x -c\nThis is a .f file.\n")
+test.must_match('test02' + _obj, " -x -c\nThis is a .F file.\n")
+test.must_match('test03' + _obj, " -x -c\nThis is a .for file.\n")
+test.must_match('test04' + _obj, " -x -c\nThis is a .FOR file.\n")
+test.must_match('test05' + _obj, " -x -c\nThis is a .ftn file.\n")
+test.must_match('test06' + _obj, " -x -c\nThis is a .FTN file.\n")
+test.must_match('test07' + _obj, " -x -c\nThis is a .fpp file.\n")
+test.must_match('test08' + _obj, " -x -c\nThis is a .FPP file.\n")
+test.must_match('test09' + _obj, " -x -c\nThis is a .f77 file.\n")
+test.must_match('test10' + _obj, " -x -c\nThis is a .F77 file.\n")
+
+
+
+g77 = test.detect('F77', 'g77')
+FTN_LIB = TestSCons.fortran_lib
+
+if g77:
+
+    test.write("wrapper.py",
+"""import os
+import string
+import sys
+open('%s', 'wb').write("wrapper.py\\n")
+os.system(string.join(sys.argv[1:], " "))
+""" % string.replace(test.workpath('wrapper.out'), '\\', '\\\\'))
+
+    test.write('SConstruct', """
+foo = Environment(LIBS = %s)
+shf77 = foo.Dictionary('SHF77')
+bar = foo.Copy(SHF77 = r'%s wrapper.py ' + shf77, SHF77FLAGS = '-Ix')
+foo.SharedLibrary(target = 'foo/foo', source = 'foo.f')
+bar.SharedLibrary(target = 'bar/bar', source = 'bar.f')
+""" % (FTN_LIB, python))
+
+    test.write('foo.f', r"""
+      PROGRAM FOO
+      PRINT *,'foo.f'
+      STOP
+      END
+""")
+
+    test.write('bar.f', r"""
+      PROGRAM BAR
+      PRINT *,'bar.f'
+      STOP
+      END
+""")
+
+
+    test.run(arguments = 'foo', stderr = None)
+
+    test.must_not_exist('wrapper.out')
+
+    test.run(arguments = 'bar')
+
+    test.must_match('wrapper.out', "wrapper.py\n")
+
+test.pass_test()
