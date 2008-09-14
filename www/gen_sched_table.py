727d0fb3b9379f5106a44c4b6f2cd577d65a4859
Greg Noel <GregNoel@tigris.org>
2008-09-14 20:40:55 +0000

    New schedule, plus machinery to maintain it

---
+++ www/gen_sched_table.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import datetime
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+	  'August', 'September', 'October', 'November', 'December']
+
+print '<table>'
+def row(*cells, **kw):
+	tr = kw.get('tr','tr')
+	print '  <tr>'
+	for cell in cells:
+		print '    <%s>%s</%s>' % (tr,cell,tr)
+	print '  </tr>'
+row('Estimated date', 'Type', 'Name', 'Comments', tr = 'th')
+
+if len(sys.argv) > 1:
+	f = open(sys.argv[1])
+else:	f = open('Schedule')
+now = None
+current = 'UNKNOWN'
+for line in f:
+	if line[0] == '#': continue	# comment
+	if line[0] == '=':
+		date,current = line[1:].strip().split()
+		now = datetime.date(*tuple([int(i) for i in date.split('-')]))
+		continue
+	if line[0] == '+':
+		incr,type,desc = line[1:].strip().split(None,2)
+		now = now + datetime.timedelta(int(incr))
+	else:
+		print 'dunna understand code', line[0]
+		sys.exit(1)
+	name = current + '.d' + str(now).replace('-','')
+	date = '%s %s %s' % (now.day,months[now.month-1],now.year)
+	if type == 'ck':
+		category = 'checkpoint'
+	elif type == 'rc':
+		category = 'candidate'
+	else:
+		current = name = type
+		category = 'release'
+	row(date, category, name, desc)
+print '</table>'
