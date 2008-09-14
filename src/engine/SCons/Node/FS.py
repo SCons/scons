4d5f54885e8aa0cbbcab6bf53553247fff61c8c3
Steven Knight <knight@baldmt.com>
2008-09-14 17:23:48 +0000

    Issue 2106:  Don't match a directory that matches the name of a #include file.  (John Gozde)

--- src/engine/SCons/Node/FS.py
+++ src/engine/SCons/Node/FS.py
@@ -2014,13 +2014,14 @@ class RootDir(Dir):
             dir_name, file_name = os.path.split(p)
             dir_node = self._lookup_abs(dir_name, Dir)
             result = klass(file_name, dir_node, self.fs)
-            self._lookupDict[k] = result
-            dir_node.entries[_my_normcase(file_name)] = result
-            dir_node.implicit = None
 
             # Double-check on disk (as configured) that the Node we
             # created matches whatever is out there in the real world.
             result.diskcheck_match()
+
+            self._lookupDict[k] = result
+            dir_node.entries[_my_normcase(file_name)] = result
+            dir_node.implicit = None
         else:
             # There is already a Node for this path name.  Allow it to
             # complain if we were looking for an inappropriate type.
