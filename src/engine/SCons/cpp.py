379fee93166e2740f09dd5c7e4479a6a1947f842
Steven Knight <knight@baldmt.com>
2008-09-23 07:08:46 +0000

    Python 1.5 compatibility:  explicity import SCons.compat (again) so the zip() function is available so the unit tests can pass when run on older Python versions.

--- src/engine/SCons/cpp.py
+++ src/engine/SCons/cpp.py
@@ -27,6 +27,11 @@ __doc__ = """
 SCons C Pre-Processor module
 """
 
+# TODO(1.5):  remove this import
+# This module doesn't use anything from SCons by name, but we import SCons
+# here to pull in zip() from the SCons.compat layer for early Pythons.
+import SCons
+
 import os
 import re
 import string
