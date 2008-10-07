0ebab9490c8566bbe0637a39b6ea4a02648715ce
Steven Knight <knight@baldmt.com>
2008-10-07 04:52:38 +0000

    Fix label placement in gnuplot graphs by the "scons-time.py func" subcommand when a profile result is equal to (or near) 0.0.

--- test/scons-time/func/file.py
+++ test/scons-time/func/file.py
@@ -74,8 +74,8 @@ vertical_bars = (
 expect2 = \
 r"""set title "ST2.CONF TITLE"
 set key bottom left
-set label 3 "label 1.5" at 0.5,1.5 right
-set label 4 "label 1.6" at 0.6,1.5 right
+set label 3 "label 1.5" at 0.5,0.5 right
+set label 4 "label 1.6" at 0.6,0.4 right
 plot '-' title "Startup" with lines lt 1, \
      '-' notitle with lines lt 7, \
      '-' title "label 1.5" with lines lt 7, \
