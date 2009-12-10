17879c1de6f589009c781e7f36e8a6a95b45c33f
Steven Knight <knight@baldmt.com>
2009-12-10 06:19:43 +0000

    Add a script for calibrating settings for timing configurations. Update the timings scripts with calibrated settings that run a full build between 9.5 and 10.0 seconds on the buildbot slave.

--- timings/CPPPATH/TimeSCons-run.py
+++ timings/CPPPATH/TimeSCons-run.py
@@ -24,15 +24,28 @@
 """
 This configuration times searching long lists of CPPPATH directories.
 
-We create 5000 on-disk directories.  A single checked-in .h file exists
-in the 'include' directory.  The SConstruct sets CPPPATH to a list of Dir
-Nodes for the created directories, followed by 'include'.  A checked-in .c
-file #includes the .h file to be found in the last directory in the list.
+We create $DIR_COUNT on-disk directories.  A single checked-in .h file
+exists in the 'include' directory.  The SConstruct sets CPPPATH to a
+list of Dir Nodes for the created directories, followed by 'include'.
+A checked-in .c file #includes the .h file to be found in the last
+directory in the list.
 """
 
 import TestSCons
 
-test = TestSCons.TimeSCons(variables={'DIR_COUNT':5000})
+# Full-build time of just under 10 seconds on ubuntu-timings slave,
+# as determined by bin/calibrate.py on 9 December 2009:
+#
+# run   1:   2.235:  DIR_COUNT=50
+# run   2:   3.976:  DIR_COUNT=223
+# run   3:   7.353:  DIR_COUNT=560
+# run   4:   9.569:  DIR_COUNT=761
+# run   5:   9.353:  DIR_COUNT=761
+# run   6:   9.972:  DIR_COUNT=813
+# run   7:   9.930:  DIR_COUNT=813
+# run   8:   9.983:  DIR_COUNT=813
+
+test = TestSCons.TimeSCons(variables={'DIR_COUNT':813})
 
 for d in xrange(test.variables['DIR_COUNT']):
     test.subdir('inc_%04d' % d)
