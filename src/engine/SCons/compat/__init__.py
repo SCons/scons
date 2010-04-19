5cb3626eef953ca08216debc3ad087ec1f513410
Steven Knight <knight@baldmt.com>
2010-04-19 05:06:30 +0000

    Remove the compat shutil.move() function.

--- src/engine/SCons/compat/__init__.py
+++ src/engine/SCons/compat/__init__.py
@@ -229,45 +229,6 @@ except ImportError:
     imp.load_module('queue', file, filename, suffix_mode_type)
 
 
-import shutil
-try:
-    shutil.move
-except AttributeError:
-    # Pre-2.3 Python has no shutil.move() function.
-    #
-    # Cribbed from Python 2.5.
-    import os
-
-    def move(src, dst):
-        """Recursively move a file or directory to another location.
-
-        If the destination is on our current filesystem, then simply use
-        rename.  Otherwise, copy src to the dst and then remove src.
-        A lot more could be done here...  A look at a mv.c shows a lot of
-        the issues this implementation glosses over.
-
-        """
-        try:
-            os.rename(src, dst)
-        except OSError:
-            if os.path.isdir(src):
-                if shutil.destinsrc(src, dst):
-                    raise Error("Cannot move a directory '%s' into itself '%s'." % (src, dst))
-                shutil.copytree(src, dst, symlinks=True)
-                shutil.rmtree(src)
-            else:
-                shutil.copy2(src,dst)
-                os.unlink(src)
-    shutil.move = move
-    del move
-
-    def destinsrc(src, dst):
-        src = os.path.abspath(src)
-        return os.path.abspath(dst)[:len(src)] == src
-    shutil.destinsrc = destinsrc
-    del destinsrc
-
-
 try:
     import subprocess
 except ImportError:
