6d84175859f58ad810c771fca08b29dbb3bad178
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2012-06-06 22:42:39 +0200

    Corrected COPYRIGH placeholders

--- src/engine/SCons/Tool/msgmerge.py
+++ src/engine/SCons/Tool/msgmerge.py
@@ -3,7 +3,7 @@
 Tool specific initialization for `msgmerge` tool.
 """
 
-# __TOOL_COPYRIGHT__
+# __COPYRIGHT__
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
--- test/XGETTEXT/POTUpdate_doc_user_examples2.py
+++ test/XGETTEXT/POTUpdate_doc_user_examples2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# __TOOL_COPYRIGHT__
+# __COPYRIGHT__
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
