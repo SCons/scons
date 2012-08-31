ca61343779372a97bea297dd4567ce234d8af407
Richard Viney <richard.viney@gmail.com>
2012-08-31 15:17:42 +1200

    Fixed error on Windows when there is a space in the Python installation directory (http://scons.tigris.org/issues/show_bug.cgi?id=2790)

--- src/script/scons.bat
+++ src/script/scons.bat
@@ -19,7 +19,7 @@ setlocal
 set path=%~dp0;%~dp0..;%path%
 @REM try the script named as the .bat file in current dir, then in Scripts subdir
 set scriptname=%~dp0%~n0.py
-if not exist %scriptname% set scriptname=%~dp0Scripts\%~n0.py
+if not exist "%scriptname%" set scriptname=%~dp0Scripts\%~n0.py
 python "%scriptname%" %*
 endlocal & set SCONS_ERRORLEVEL=%ERRORLEVEL%
 
