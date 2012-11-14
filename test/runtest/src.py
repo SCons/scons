b8e5ae03dbec474a61ae14940ddfb9def184cfc7
Juan Lang <juanlang@google.com>
2012-11-14 13:01:08 -0800

    Use .wixobj rather than .wxiobj as the suffix for WiX compiler output

---
+++ test/runtest/src.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# __COPYRIGHT__
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
+
+"""
+Verify that we find tests under the src/ tree only if they end
+with *Tests.py.
+"""
+
+import os
+
+import TestRuntest
+
+test = TestRuntest.TestRuntest()
+
+test.subdir(['src'],
+            ['src', 'suite'])
+
+pythonstring = TestRuntest.pythonstring
+src_passTests_py = os.path.join('src', 'passTests.py')
+src_suite_passTests_py = os.path.join('src', 'suite', 'passTests.py')
+
+test.write_passing_test(['src', 'pass.py'])
+
+test.write_passing_test(['src', 'passTests.py'])
+
+test.write_passing_test(['src', 'suite', 'pass.py'])
+
+test.write_passing_test(['src', 'suite', 'passTests.py'])
+
+expect_stdout = """\
+%(pythonstring)s -tt %(src_passTests_py)s
+PASSING TEST STDOUT
+%(pythonstring)s -tt %(src_suite_passTests_py)s
+PASSING TEST STDOUT
+""" % locals()
+
+expect_stderr = """\
+PASSING TEST STDERR
+PASSING TEST STDERR
+""" % locals()
+
+test.run(arguments='-k src', stdout=expect_stdout, stderr=expect_stderr)
+
+test.pass_test()
+
+# Local Variables:
+# tab-width:4
+# indent-tabs-mode:nil
+# End:
+# vim: set expandtab tabstop=4 shiftwidth=4:
---
+++ www/gen_sched_table.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import datetime
+
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+	  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+print '<table width="100%">'
+def row(*cells, **kw):
+	td = kw.get('tr','td')
+	print '  <tr>'
+	for cell in cells:
+		print '    <%s>%s</%s>' % (td,cell,td)
+	print '  </tr>'
+row('Estimated&nbsp;date', 'Type', 'Comments', tr = 'th')
+
+if len(sys.argv) > 1:
+	f = open(sys.argv[1])
+else:	f = open('schedule')
+now = None
+current = 'UNKNOWN'
+for line in f:
+	if line[0] == '#': continue	# comment
+	if line[0] == '=':
+		date,current = line[1:].strip().split(None, 1)
+		now = datetime.date(*tuple([int(i) for i in date.split('-')]))
+		continue
+	if line[0] == '+':
+		incr,type,desc = line[1:].strip().split(None,2)
+		now = now + datetime.timedelta(int(incr))
+	else:
+		print 'dunna understand code', line[0]
+		sys.exit(1)
+	#name = current + '.d' + str(now).replace('-','')
+	date = '%s-%s-%s' % (now.day,months[now.month-1],now.year)
+	if type == 'ck':
+		category = 'Ckpt'
+	elif type == 'rc':
+		category = 'RC'
+	else:
+		category = current = type
+	row(date, category, desc)
+print '</table>'
+
+# Local Variables:
+# tab-width:4
+# indent-tabs-mode:nil
+# End:
+# vim: set expandtab tabstop=4 shiftwidth=4:
