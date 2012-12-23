51251b0379de83be950be06203c4840e17e0de88
GaryO <139975+garyo@users.noreply.github.com>
2012-12-23 14:13:56 -0500

    Fix MSVS project generation for older (<10) MSVS versions.

--- src/engine/SCons/Tool/msvs.py
+++ src/engine/SCons/Tool/msvs.py
@@ -1655,7 +1655,7 @@ def projectEmitter(target, source, env):
         targetlist = targetlist + t
 
     # Beginning with Visual Studio 2010 for each project file (.vcxproj) we have additional file (.vcxproj.filters)
-    if env['MSVS_VERSION'] >= 10.0:
+    if float(env['MSVS_VERSION']) >= 10.0:
         targetlist.append(targetlist[0] + '.filters')
 
     return (targetlist, sourcelist)
