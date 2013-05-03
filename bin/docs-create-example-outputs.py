6885c3fd4f501aff9d5cde7b0bb3aedaf5b63e10
Dirk Baechle <dl9obn@darc.de>
2013-05-04 00:41:56 +0200

    - cleaned up 'bin' directory - several smaller fixes - added first version of the Overview document for the doc toolchain

--- bin/docs-create-example-outputs.py
+++ bin/docs-create-example-outputs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 #
-# Searches through the whole doc/user tree and verifies
-# that the names of the single examples are unique over
-# all *.xml files.
-# Additionally, the suffix entries have to be unique
-# within each scons_command_output.
+# Searches through the whole doc/user tree and creates
+# all output files for the single examples.
 #
 
 import os
+import sys
 import SConsExamples
 
 if __name__ == "__main__":
+    print "Checking whether all example names are unique..."
     if SConsExamples.exampleNamesAreUnique(os.path.join('doc','user')):
         print "OK"
     else:
         print "Not all example names and suffixes are unique! Please correct the errors listed above and try again."
+        sys.exit(0)
+    
+    SConsExamples.createAllExampleOutputs(os.path.join('doc','user'))
