9f36c5b899b8d2d54cae8d3da76b01308c144ed6
ndbecker2 <ndbecker2@gmail.com>
2013-09-10 10:02:23 -0400

    setup.py: don't error on py3 (transplanted from 16fc64cadd68f2f9032c14d04bc0bf94f99b71a4)

--- src/setup.py
+++ src/setup.py
@@ -46,14 +46,6 @@ man_pages = [
     'scons-time.1',
 ]
 
-# Exit with error if trying to install with Python >= 3.0
-if sys.version_info >= (3,0,0):
-    msg = "scons: *** SCons does not run under Python version %s.\n\
-Python 3 and above are not yet supported.\n"
-    sys.stderr.write(msg % (sys.version.split()[0]))
-    sys.exit(1)
-
-
 # change to setup.py directory if it was executed from other dir
 (head, tail) = os.path.split(sys.argv[0])
 if head:
