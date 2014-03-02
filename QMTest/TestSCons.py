236534f6d1ec353428b5fd9174a6c4a933d409fc
GaryO <139975+garyo@users.noreply.github.com>
2014-03-02 17:36:36 -0500

    Final auto updates for 2.3.1 release

--- QMTest/TestSCons.py
+++ QMTest/TestSCons.py
@@ -34,7 +34,7 @@ from TestCmd import PIPE
 # here provides some independent verification that what we packaged
 # conforms to what we expect.
 
-default_version = '2.3.1.alpha.yyyymmdd'
+default_version = '2.3.1'
 
 python_version_unsupported = (2, 3, 0)
 python_version_deprecated = (2, 7, 0)
