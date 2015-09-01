a6a65ee80272d61fa2e09e33eeedfa0e08aed333
Dirk Baechle <dl9obn@darc.de>
2015-09-01 23:09:11 +0200

    - added "suffix" attribute to backward compat layer (getattr) for Node.FS entries

--- src/engine/SCons/Node/FS.py
+++ src/engine/SCons/Node/FS.py
@@ -664,8 +664,8 @@ class Base(SCons.Node.Node):
         """ Together with the node_bwcomp dict defined below,
             this method provides a simple backward compatibility
             layer for the Node attributes 'abspath', 'labspath',
-            'path', 'tpath' and 'path_elements'. These Node attributes
-            used to be directly available in v2.3 and earlier, but
+            'path', 'tpath', 'suffix' and 'path_elements'. These Node
+            attributes used to be directly available in v2.3 and earlier, but
             have been replaced by getter methods that initialize the
             single variables lazily when required, in order to save memory.
             The redirection to the getters lets older Tools and
@@ -948,7 +948,8 @@ node_bwcomp = {'abspath' : Base.get_abspath,
                'labspath' : Base.get_labspath,
                'path' : Base.get_internal_path,
                'tpath' : Base.get_tpath,
-               'path_elements' : Base.get_path_elements}
+               'path_elements' : Base.get_path_elements,
+               'suffix' : Base.get_suffix}
 
 class Entry(Base):
     """This is the class for generic Node.FS entries--that is, things
