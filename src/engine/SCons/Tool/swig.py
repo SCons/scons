98b9ca9a3abd8c5c5fae9ff92331b63ba4cedd87
William Deegan <bill@baddogconsulting.com>
2015-12-17 09:31:03 -0500

    fix swig test failing on windows due to  \r\n line endings

--- src/engine/SCons/Tool/swig.py
+++ src/engine/SCons/Tool/swig.py
@@ -139,9 +139,13 @@ def _get_swig_version(env, swig):
     if pipe.wait() != 0: return
 
     out = pipe.stdout.read()
-    match = re.search(r'SWIG Version\s+(\S+)$', out, re.MULTILINE)
+    match = re.search(r'SWIG Version\s+(\S+).*', out, re.MULTILINE)
     if match:
+        # print "Version is:%s"%match.group(1)
         return match.group(1)
+    else:
+        # print "Unable to detect version: [%s]"%out
+        pass
 
 def generate(env):
     """Add Builders and construction variables for swig to an Environment."""
