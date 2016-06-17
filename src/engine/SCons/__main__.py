16c3e45d786b939f78cf0169be160fd65c679150
Daniel Holth <dholth@fastmail.fm>
2016-06-16 22:49:38 -0400

    no need for a __main__ guard in __main__.py

--- src/engine/SCons/__main__.py
+++ src/engine/SCons/__main__.py
@@ -1,5 +1,4 @@
-if __name__ == "__main__":
-    import SCons.Script
-    # this does all the work, and calls sys.exit
-    # with the proper exit status when done.
-    SCons.Script.main()
+import SCons.Script
+# this does all the work, and calls sys.exit
+# with the proper exit status when done.
+SCons.Script.main()
