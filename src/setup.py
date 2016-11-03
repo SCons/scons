9d637bc1d28420698f4a9345b7579d09cc8c311f
William Deegan <bill@baddogconsulting.com>
2016-11-03 16:25:46 -0400

    manually merge packaging updates from 2.5.1

--- src/setup.py
+++ src/setup.py
@@ -388,6 +388,7 @@ scripts = [
     'script/scons',
     'script/sconsign',
     'script/scons-time',
+    'script/scons-configure-cache',
 
     # We include scons.bat in the list of scripts, even on UNIX systems,
     # because we provide an option to allow it be installed explicitly,
