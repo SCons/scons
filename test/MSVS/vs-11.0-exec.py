b527c93ebbb5b3e83b2f0882cf62d6072d8b4cb5
Daniel Moody <dmoody256@gmail.com>
2016-11-11 01:19:20 -0500

    The percent signs are causing syntax error on windows, switched to more compatible code

--- test/MSVS/vs-11.0-exec.py
+++ test/MSVS/vs-11.0-exec.py
@@ -55,7 +55,8 @@ if not msvs_version in test.msvs_versions():
 
 test.run(arguments = '-n -q -Q -f -', stdin = """\
 env = Environment(tools = ['msvc'], MSVS_VERSION='%(msvs_version)s')
-print "os.environ.update(%%s)" %% repr(env['ENV'])
+sconsEnv = repr(env['ENV'])
+print("os.environ.update(" + sconsEnv + ")")
 """ % locals())
 
 exec(test.stdout())
