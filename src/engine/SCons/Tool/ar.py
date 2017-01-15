7b514fb2f9cf65c0fcc6b719d788a20fbad6104f
William Deegan <bill@baddogconsulting.com>
2017-01-14 18:19:49 -0800

    Fix merge errors

--- src/engine/SCons/Tool/ar.py
+++ src/engine/SCons/Tool/ar.py
@@ -48,11 +48,7 @@ def generate(env):
     env['LIBPREFIX']   = 'lib'
     env['LIBSUFFIX']   = '.a'
 
-<<<<<<< working copy
-    if env.get('RANLIB',False) or env.Detect('ranlib'):
-=======
     if env.get('RANLIB',env.Detect('ranlib')) :
->>>>>>> merge rev
         env['RANLIB']      = env.get('RANLIB','ranlib')
         env['RANLIBFLAGS'] = SCons.Util.CLVar('')
         env['RANLIBCOM']   = '$RANLIB $RANLIBFLAGS $TARGET'
