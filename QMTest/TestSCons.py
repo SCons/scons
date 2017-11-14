0b8a2ac3b7f2eea24a374600ac617a76c6c1cae1
William Deegan <bill@baddogconsulting.com>
2017-11-14 18:11:50 -0500

    change back to development version strings

--- QMTest/TestSCons.py
+++ QMTest/TestSCons.py
@@ -35,7 +35,7 @@ from TestCmd import PIPE
 # here provides some independent verification that what we packaged
 # conforms to what we expect.
 
-default_version = '3.0.1'
+default_version = '3.1.0.alpha.yyyymmdd'
 
 python_version_unsupported = (2, 6, 0)
 python_version_deprecated = (2, 7, 0)
