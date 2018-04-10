de0d6f38404c6880e4b401f7ea6fd629bcb765ea
Daniel Moody <dmoody256@gmail.com>
2018-04-10 08:03:43 -0500

    removed binary read and write so newlines are crossplatform and updated path for appveyor script

--- test/YACC/YACC-fixture/myyacc.py
+++ test/YACC/YACC-fixture/myyacc.py
@@ -4,10 +4,10 @@ cmd_opts, args = getopt.getopt(sys.argv[1:], 'o:', [])
 output = None
 opt_string = ''
 for opt, arg in cmd_opts:
-    if opt == '-o': output = open(arg, 'wb')
+    if opt == '-o': output = open(arg, 'w')
     else: opt_string = opt_string + ' ' + opt
 for a in args:
-    contents = open(a, 'rb').read()
-    output.write(contents.replace(b'YACC', b'myyacc.py'))
+    contents = open(a, 'r').read()
+    output.write(contents.replace('YACC', 'myyacc.py'))
 output.close()
 sys.exit(0)
