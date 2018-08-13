d8ecd073646fce63d24e0a4bab16edb4f2ce9cf6
Daniel Moody <dmoody256@gmail.com>
2018-08-13 09:51:09 -0500

    Updating a java test to add quotes to the path

--- test/Java/JARCHDIR.py
+++ test/Java/JARCHDIR.py
@@ -42,6 +42,8 @@ test = TestSCons.TestSCons()
 where_javac, java_version = test.java_where_javac()
 where_jar = test.java_where_jar()
 
+where_javac = '"' + where_javac + '"'
+where_jar = '"' + where_jar + '"'
 
 
 test.write('SConstruct', """
