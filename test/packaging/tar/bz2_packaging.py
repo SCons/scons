bbde3d2bbf2a7e34d369c5be069fde2d5e235670
Mats Wichmann <mats@linux.com>
2018-09-07 07:37:58 -0600

    Improvements to tarball packager
    Both gz and bzip2 now skip the test if tar is not found (previously they
    would not test, but mark the test OK).
    
    Since tar is now found on recent Windows 10, but the helper program
    bzip2 is not, check for bzip2 as well.  This is actually a correct test
    for other systems as well, they have just been very unlikely to not have
    had bzip2 provisioned so it has not bitten us.
    
    Note: on Windows, with cygwin installed, the test may well find bzip2
    but c:\Windows\System32\tar.exe does not find it and the test fails
    anyway. Work needed on this "finds too much" issue (not unique to
    this test).
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- test/packaging/tar/bz2_packaging.py
+++ test/packaging/tar/bz2_packaging.py
@@ -36,18 +36,23 @@ python = TestSCons.python
 test = TestSCons.TestSCons()
 
 tar = test.detect('TAR', 'tar')
+if not tar:
+    test.skip_test('tar not found, skipping test\n')
 
-if tar:
-  test.subdir('src')
+bz2 = test.where_is('bzip2')
+if not bz2:
+    test.skip_test('tar found, but helper bzip2 not found, skipping test\n')
 
-  test.write( [ 'src', 'main.c' ], r"""
+test.subdir('src')
+
+test.write([ 'src', 'main.c'], r"""
 int main( int argc, char* argv[] )
 {
   return 0;
 }
 """)
 
-  test.write('SConstruct', """
+test.write('SConstruct', """
 Program( 'src/main.c' )
 env=Environment(tools=['default', 'packaging'])
 env.Package( PACKAGETYPE  = 'src_tarbz2',
@@ -56,9 +61,9 @@ env.Package( PACKAGETYPE  = 'src_tarbz2',
              source       = [ 'src/main.c', 'SConstruct' ] )
 """)
 
-  test.run(arguments='', stderr = None)
+test.run(arguments='', stderr=None)
 
-  test.must_exist( 'src.tar.bz2' )
+test.must_exist('src.tar.bz2')
 
 test.pass_test()
 
