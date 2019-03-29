4baba18b6012b88a556aefc5183bc2486375e970
Mats Wichmann <mats@linux.com>
2019-03-29 09:22:00 -0600

    another experiment
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- testing/framework/TestCmd.py
+++ testing/framework/TestCmd.py
@@ -366,11 +366,9 @@ else:
     def is_String(e):
         return isinstance(e, (str, unicode, UserString))
 
-tempfile.template = 'testcmd.'
+testprefix = 'testcmd.'
 if os.name in ('posix', 'nt'):
-    tempfile.template = 'testcmd.' + str(os.getpid()) + '.'
-else:
-    tempfile.template = 'testcmd.'
+    testprefix += "%s." % str(os.getpid()
 
 re_space = re.compile('\s')
 
@@ -1662,10 +1660,11 @@ class TestCmd(object):
         """
         if path is None:
             try:
-                path = tempfile.mktemp(prefix=tempfile.template)
+                path = tempfile.mkdtemp(prefix=testprefix)
             except TypeError:
-                path = tempfile.mktemp()
-        os.mkdir(path)
+                path = tempfile.mkdtemp()
+        else
+            os.mkdir(path)
 
         # Symlinks in the path will report things
         # differently from os.getcwd(), so chdir there
