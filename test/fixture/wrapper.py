2931eea70b0234cf284c88b9a2a31e1c63bcfcd6
Mats Wichmann <mats@linux.com>
2020-02-13 08:16:11 -0700

    [PR #3554] restore wrapper.py for former state
    Changes didn't work on Windows.
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- test/fixture/wrapper.py
+++ test/fixture/wrapper.py
@@ -5,6 +5,6 @@ import subprocess
 if __name__ == '__main__':
     path = os.path.join(os.path.dirname(os.path.relpath(__file__)), 'wrapper.out')
     if '--version' not in sys.argv and '-dumpversion' not in sys.argv:
-        with open(path, 'w') as f:
-            f.write("wrapper.py\n")
+        with open(path, 'wb') as f:
+            f.write(b"wrapper.py\n")
     subprocess.run(sys.argv[1:])
