2003f3b5c098b27a70952bfc1847f4140cb2afec
Paul Tipei <paul.bogdan.tipei@intel.com>
2020-02-19 17:07:16 +0200

    updated the option-tree.py file

--- test/option--tree.py
+++ test/option--tree.py
@@ -61,10 +61,7 @@ except NameError:
 env.Textfile("Foo", write)
 """)
 
-if sys.version_info.major < 3:
-    py23_char = unichr(0xe7).encode('utf-8')
-else:
-    py23_char = chr(0xe7)
+py23_char = chr(0xe7)
 
 expected = """Creating 'Foo.txt'
 +-.
