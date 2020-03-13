3ac4e188a7d00449ad22ef690384eae422783376
Mats Wichmann <mats@linux.com>
2020-03-13 08:45:22 -0600

    Fix the packagers table again.  [ci skip]
    Trying yet another layout to make it look better.
    
    Minor tweaks to the doc production tools - dropping some very
    old usage (pre-Python-2.5).
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- bin/SConsDoc.py
+++ bin/SConsDoc.py
@@ -141,22 +141,12 @@ if not has_libxml2:
         pass
 if not has_etree:
     try:
-        # Python 2.5
         import xml.etree.cElementTree as etree
     except ImportError:
         try:
-            # Python 2.5
             import xml.etree.ElementTree as etree
         except ImportError:
-            try:
-                # normal cElementTree install
-                import cElementTree as etree
-            except ImportError:
-                try:
-                    # normal ElementTree install
-                    import elementtree.ElementTree as etree
-                except ImportError:
-                    raise ImportError("Failed to import ElementTree from any known place")
+            raise ImportError("Failed to import ElementTree from any known place")
 
 # patterns to help trim XML passed in as strings
 re_entity = re.compile(r"&([^;]+);")
