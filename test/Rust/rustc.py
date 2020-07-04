0fccaaf72efe95bd9811c900a8f4f74499780483
Lior Stern <liorst4@gmail.com>
2020-07-04 14:10:03 +0300

    Add a basic hello world test to rust.

---
+++ test/Rust/rustc.py
@@ -0,0 +1,37 @@
+#
+# __COPYRIGHT__
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+import TestSCons
+
+test = TestSCons.TestSCons()
+test.write("hello.rs", r"""
+fn main() {
+    println!("{}", "Hello world!");
+}
+""")
+test.write('SConstruct', r"""
+env = Environment()
+env.Program("hello", "hello.rs")
+""")
+test.run("hello")
+# TODO: Check the output of hello world
