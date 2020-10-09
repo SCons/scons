428382dc6ee5f7d90affa6638045f2ea20be5b4d
William Deegan <bill@baddogconsulting.com>
2020-10-09 13:22:10 -0700

    Fix test

--- .travis/verify_packages.sh
+++ .travis/verify_packages.sh
@@ -2,17 +2,31 @@
 set -e
 set -x
 
+retval=0
+expected_man_file_count=3
 echo "Checking wheel file"
-wheel_man_files=$(unzip -l build/dist/SCons-*-py3-none-any.whl | grep -e '[a-z].1$' | tee /dev/stderr | wc -l)
+wheel_man_files=$(unzip -l build/dist/SCons-*-py3-none-any.whl | grep -e '[a-z].1$' | wc -l | xargs)
 echo "Number of manpage files: $wheel_man_files"
 
 echo "Checking tgz sdist package"
-tgz_man_files=$(tar tvfz build/dist/SCons-*.tar.gz | grep -e '[a-z].1$' | tee /dev/stderr | wc -l)
+tgz_man_files=$(tar tvfz build/dist/SCons-*.tar.gz | grep -e '[a-z].1$' | wc -l |xargs)
 
 echo "Checking zip sdist package"
-zip_man_files=$(unzip -l build/dist/SCons-*.zip | grep -e '[a-z].1$' | tee /dev/stderr | wc -l)
+zip_man_files=$(unzip -l build/dist/SCons-*.zip | grep -e '[a-z].1$' | wc -l |xargs)
 
-if [[ $wheel_man_files =~ /\\s+4/ ]]; then
+if [[ $wheel_man_files != $expected_man_file_count ]]; then
    echo "Manpages not in wheel"
-   exit 1
+   retval=1
 fi
+
+if [[ $tgz_man_files != $expected_man_file_count ]]; then
+   echo "Manpages not in tgz sdist package"
+   retval=2
+fi
+
+if [[ $zip_man_files != $expected_man_file_count ]]; then
+   echo "Manpages not in zip sdist package"
+   retval=3
+fi
+
+exit $retval
