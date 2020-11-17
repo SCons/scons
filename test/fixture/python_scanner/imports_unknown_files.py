a6f317b4301d968cbda2c7fbe903d23347900bf8
Adam Gross <grossag@vmware.com>
2020-11-17 15:31:06 -0500

    [ci skip] Fix flake8 warnings, add description to CHANGES.txt

--- test/fixture/python_scanner/imports_unknown_files.py
+++ test/fixture/python_scanner/imports_unknown_files.py
@@ -1,3 +1,3 @@
-import doesntexist
-import notthere.something
-from notthere import a, few, things
+import doesntexist  # noqa: F401
+import notthere.something  # noqa: F401
+from notthere import a, few, things  # noqa: F401
