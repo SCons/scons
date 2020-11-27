17097d79ac67adff904deae714ec024f497efcdd
Mats Wichmann <mats@linux.com>
2020-11-27 12:00:25 -0700

    Fix botched checkin  [skip appveyor]
    Checked in the SCons/__init__.py instead of the changes to the
    sconsign manpage. Restoring the former and updating the latter.
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.0.1.9998"
 __copyright__="Copyright (c) 2001 - 2020 The SCons Foundation"
-__developer__="mats"
-__date__="2020-11-26 16:47:04"
-__buildsys__="boulder"
-__revision__="64a80e530b0caef9763eb67bd770dd9c8925ea18"
-__build__="64a80e530b0caef9763eb67bd770dd9c8925ea18"
+__developer__="bdbaddog"
+__date__="2020-10-09 19:00:35"
+__buildsys__="ProDog2020"
+__revision__="93525bed88d19a00f5de400086c0046011d3b833"
+__build__="93525bed88d19a00f5de400086c0046011d3b833"
 # make sure compatibility is always in place
 import SCons.compat # noqa
