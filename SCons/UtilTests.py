68e4dc36483ad8c0c1757394b6380410ed665e10
William Deegan <bill@baddogconsulting.com>
2020-12-12 17:30:08 -0800

    Fix sider complaints

--- SCons/UtilTests.py
+++ SCons/UtilTests.py
@@ -21,7 +21,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from collections import UserDict, UserList, UserString, OrderedDict
+from collections import UserDict, UserList, UserString
 import functools
 import io
 import os
