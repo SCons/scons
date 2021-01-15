48ea8aad37fc9c0196bb95b299809e6c53d4a012
William Deegan <bill@baddogconsulting.com>
2021-01-15 19:58:15 +0000

    Add generating versioned shared library to D SharedObject tests (in addition to existing plain shared library test.

--- test/D/SharedObjects/Common/common.py
+++ test/D/SharedObjects/Common/common.py
@@ -1,6 +1,3 @@
-"""
-Support functions for all the tests.
-"""
 # MIT License
 #
 # Copyright The SCons Foundation
@@ -23,6 +20,10 @@ Support functions for all the tests.
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+Support functions for all the tests.
+"""
+
 
 import TestSCons
 
