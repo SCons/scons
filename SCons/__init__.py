dc58c175da659d6c0bb3e049ba56fb42e77546cd
William Deegan <bill@baddogconsulting.com>
2021-01-19 12:10:56 -0800

    update API doc version to 4.1.0 and add some more dependencies on the api target to include the sphinx config files

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.1.0"
 __copyright__="Copyright (c) 2001 - 2021 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="2021-01-19 18:19:30"
+__date__="2021-01-19 20:03:01"
 __buildsys__="ProDog2020"
-__revision__="aee5793fdea4cc6a31e5346ddbd847c9ba23d64d"
-__build__="aee5793fdea4cc6a31e5346ddbd847c9ba23d64d"
+__revision__="215860fd4f6bea67896c145660a035fad20cc41c"
+__build__="215860fd4f6bea67896c145660a035fad20cc41c"
 # make sure compatibility is always in place
 import SCons.compat # noqa
