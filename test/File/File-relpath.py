39eed7be98e5516429d4dfb740090ba8c5619b76
William Deegan <bill@baddogconsulting.com>
2021-03-25 17:27:54 -0700

    Simplify test and fix to work on all platforms

--- test/File/File-relpath.py
+++ test/File/File-relpath.py
@@ -31,18 +31,32 @@ Specifically ${TARGET.relpath}, ${SOURCE.relpath} match expected path
 import os
 
 import TestSCons
+from TestCmd import IS_WINDOWS
 
 test = TestSCons.TestSCons()
 
-test.subdir('src', ['src', 'dir'])
+test.subdir("src", ["src", "dir"])
 
-test.dir_fixture('fixture/relpath')
-test.run('-Q', chdir='base', status=0, stdout="""\
-../foo/dir build/file1
-%s %s
-src/file
-%s
-src/file
-%s
-""" % (os.path.abspath('base/../foo/dir'), os.path.abspath('base/build/file1'), os.path.abspath('base/src/file'),
-       os.path.abspath('base/src/file')))
+test.dir_fixture("fixture/relpath")
+
+expected = [
+       # expanding variable, expected string
+    ("${TARGETS.relpath}", "../foo/dir build/file1"),
+    (
+        "${TARGETS.abspath}",
+        "%s %s"
+        % (os.path.abspath("base/../foo/dir"), os.path.abspath("base/build/file1")),
+    ),
+    ("${SOURCES.relpath}", "src/file"),
+    ("${SOURCES.abspath}", os.path.abspath("base/src/file")),
+    ("${SOURCE.relpath}", "src/file"),
+    ("${SOURCE.abspath}", os.path.abspath("base/src/file")),
+]
+
+expected_stdout="\n".join(["%s=%s"%(s,o) for s,o in expected])
+expected_stdout+="\nscons: `.' is up to date."
+
+if IS_WINDOWS:
+       expected_stdout = expected_stdout.replace('/', os.sep)
+
+test.run('-Q', chdir='base', status=0, stdout=expected_stdout+"\n")
