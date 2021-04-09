dd2b96c596b9446c356aea5f9fa41aab235bc24d
Daniel Moody <daniel.moody@mongodb.com>
2021-04-09 16:54:50 +0000

    Dont call progress if the progress object is Null

--- SCons/Script/Main.py
+++ SCons/Script/Main.py
@@ -173,8 +173,9 @@ class BuildTask(SCons.Taskmaster.OutOfDateTask):
         display('scons: ' + message)
 
     def prepare(self):
-        for target in self.targets:
-            self.progress(target)
+        if not isinstance(self.progress, SCons.Util.Null):
+            for target in self.targets:
+                self.progress(target)
         return SCons.Taskmaster.OutOfDateTask.prepare(self)
 
     def needs_execute(self):
