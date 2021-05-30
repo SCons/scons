ceba1ffedee631797f91b811dfb3bd197c92efce
William Deegan <bill@baddogconsulting.com>
2021-05-30 14:42:27 -0700

    Address mwichmann's comments on PR. Mostly doc and a few import related code changes

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.1.1a"
 __copyright__="Copyright (c) 2001 - 2021 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="2021-04-18 22:22:37"
+__date__="2021-05-30 21:33:41"
 __buildsys__="ProDog2020"
-__revision__="1850bdd13c7668aac493b9f5b896578813e60ca9"
-__build__="1850bdd13c7668aac493b9f5b896578813e60ca9"
+__revision__="b4c7a0966c36d37d0c2ea125dd98bff0061e28f2"
+__build__="b4c7a0966c36d37d0c2ea125dd98bff0061e28f2"
 # make sure compatibility is always in place
 import SCons.compat # noqa
