43e1da1f024dc8185a095b9d1e3058b8a4b6ab35
William Deegan <bill@baddogconsulting.com>
2021-07-31 20:08:37 -0700

    final versioning info for 4.2.0

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.2.0"
 __copyright__="Copyright (c) 2001 - 2021 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="Sun, 18 Jul 2021 20:21:57 -0700"
+__date__="Sat, 31 Jul 2021 18:12:46 -0700"
 __buildsys__="ProDog2020"
-__revision__="801acd51da9cfd64b8323828c072a1f353323da9"
-__build__="801acd51da9cfd64b8323828c072a1f353323da9"
+__revision__="fcdadeef19fe5fead09fa7544a27502be65312be"
+__build__="fcdadeef19fe5fead09fa7544a27502be65312be"
 # make sure compatibility is always in place
 import SCons.compat # noqa
