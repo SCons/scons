5c0d91c3a38effd1ce4305e71168ea9e924437cb
William Deegan <bill@baddogconsulting.com>
2021-11-01 12:28:12 -0700

    fixed tests to test --no-ignore-skips, and updated their copyright text

--- test/runtest/simple/no_result.py
+++ test/runtest/simple/no_result.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -20,9 +22,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 """
 Test how we handle a no-results test specified on the command line.
@@ -48,10 +47,16 @@ expect_stderr = """\
 NO RESULT TEST STDERR
 """
 
-test.run(arguments='-k test/no_result.py',
+test.run(arguments='--no-ignore-skips -k test/no_result.py',
          status=2,
          stdout=expect_stdout,
          stderr=expect_stderr)
+
+test.run(arguments='-k test/no_result.py',
+         status=0,
+         stdout=expect_stdout,
+         stderr=expect_stderr)
+
 
 test.pass_test()
 
