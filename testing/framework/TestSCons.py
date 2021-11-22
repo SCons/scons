ea681d9b3d15acb1b9f2cd029be5a886082b255b
William Deegan <bill@baddogconsulting.com>
2021-11-21 17:16:00 -0800

    fix some issues with current version strings

--- testing/framework/TestSCons.py
+++ testing/framework/TestSCons.py
@@ -55,7 +55,7 @@ from TestCmd import PIPE
 # here provides some independent verification that what we packaged
 # conforms to what we expect.
 
-default_version = '4.3.0ayyyymmdd'
+default_version = '4.3.1ayyyymmdd'
 
 # TODO: these need to be hand-edited when there are changes
 python_version_unsupported = (3, 6, 0)
