2b673e30e81f83304746e7fec9aa4ddb0fd66f88
Mats Wichmann <mats@linux.com>
2022-03-22 14:11:13 -0600

    Fix some more tests to use context-mgr open
    Fiddled formatting a bit.
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- test/option/option-i.py
+++ test/option/option-i.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -20,9 +22,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 import os.path
 
@@ -34,43 +33,46 @@ test = TestSCons.TestSCons()
 
 test.write('succeed.py', r"""
 import sys
-file = open(sys.argv[1], 'w')
-file.write("succeed.py: %s\n" % sys.argv[1])
-file.close()
+
+with open(sys.argv[1], 'w') as file:
+    file.write("succeed.py: %s\n" % sys.argv[1])
 sys.exit(0)
 """)
 
 test.write('fail.py', r"""
 import sys
+
 sys.exit(1)
 """)
 
 test.write('SConstruct', """
-Succeed = Builder(action = r'%(_python_)s succeed.py $TARGETS')
-Fail = Builder(action = r'%(_python_)s fail.py $TARGETS')
-env = Environment(BUILDERS = { 'Succeed' : Succeed, 'Fail' : Fail })
-env.Fail(target = 'aaa.1', source = 'aaa.in')
-env.Succeed(target = 'aaa.out', source = 'aaa.1')
-env.Fail(target = 'bbb.1', source = 'bbb.in')
-env.Succeed(target = 'bbb.out', source = 'bbb.1')
+Succeed = Builder(action=r'%(_python_)s succeed.py $TARGETS')
+Fail = Builder(action=r'%(_python_)s fail.py $TARGETS')
+env = Environment(BUILDERS={'Succeed': Succeed, 'Fail': Fail})
+env.Fail(target='aaa.1', source='aaa.in')
+env.Succeed(target='aaa.out', source='aaa.1')
+env.Fail(target='bbb.1', source='bbb.in')
+env.Succeed(target='bbb.out', source='bbb.1')
 """ % locals())
 
 test.write('aaa.in', "aaa.in\n")
 test.write('bbb.in', "bbb.in\n")
 
-test.run(arguments = 'aaa.1 aaa.out bbb.1 bbb.out',
-         stderr = 'scons: *** [aaa.1] Error 1\n',
-         status = 2)
+test.run(
+    arguments='aaa.1 aaa.out bbb.1 bbb.out',
+    stderr='scons: *** [aaa.1] Error 1\n',
+    status=2,
+)
 
 test.fail_test(os.path.exists(test.workpath('aaa.1')))
 test.fail_test(os.path.exists(test.workpath('aaa.out')))
 test.fail_test(os.path.exists(test.workpath('bbb.1')))
 test.fail_test(os.path.exists(test.workpath('bbb.out')))
 
-test.run(arguments = '-i aaa.1 aaa.out bbb.1 bbb.out',
-         stderr =
-         'scons: *** [aaa.1] Error 1\n'
-         'scons: *** [bbb.1] Error 1\n')
+test.run(
+    arguments='-i aaa.1 aaa.out bbb.1 bbb.out',
+    stderr='scons: *** [aaa.1] Error 1\nscons: *** [bbb.1] Error 1\n',
+)
          
 test.fail_test(os.path.exists(test.workpath('aaa.1')))
 test.fail_test(test.read('aaa.out',mode='r') != "succeed.py: aaa.out\n")
@@ -80,9 +82,10 @@ test.fail_test(test.read('bbb.out',mode='r') != "succeed.py: bbb.out\n")
 test.unlink("aaa.out")
 test.unlink("bbb.out")
 
-test.run(arguments='--ignore-errors aaa.1 aaa.out bbb.1 bbb.out',
-         stderr='scons: *** [aaa.1] Error 1\n'
-         'scons: *** [bbb.1] Error 1\n')
+test.run(
+    arguments='--ignore-errors aaa.1 aaa.out bbb.1 bbb.out',
+    stderr='scons: *** [aaa.1] Error 1\nscons: *** [bbb.1] Error 1\n',
+)
 
 test.fail_test(os.path.exists(test.workpath('aaa.1')))
 test.fail_test(test.read('aaa.out', mode='r') != "succeed.py: aaa.out\n")
