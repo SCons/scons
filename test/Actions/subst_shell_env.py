70d87f61fbd923cb79dd9e37a4580b2f2dcfb000
Daniel Moody <dmoody256@gmail.com>
2022-05-10 13:39:41 -0500

    updated test to test expanding differently in different actions

--- test/Actions/subst_shell_env.py
+++ test/Actions/subst_shell_env.py
@@ -37,6 +37,7 @@ test.dir_fixture('subst_shell_env-fixture')
 
 test.run(arguments = ['-Q'])
 test.must_match('out.txt', f"I_got_expanded_to_out.txt{os.linesep}$EXPAND_THIS{os.linesep}")
+test.must_match('out2.txt', f"I_got_expanded_to_out2.txt{os.linesep}$EXPAND_THIS{os.linesep}")
 
 test.pass_test()
 
