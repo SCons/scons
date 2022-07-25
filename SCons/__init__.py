94133d4d3e63185f2649cc231ed182eba6df29d4
William Deegan <bill@baddogconsulting.com>
2022-07-24 18:06:05 -0700

    Fix sphinx version < 5.1.0. For now 5.1.0 breaks the sphinx api build

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
-__version__="4.3.0"
-__copyright__="Copyright (c) 2001 - 2021 The SCons Foundation"
-__developer__="bdeegan"
-__date__="Tue, 16 Nov 2021 19:09:21 +0000"
-__buildsys__="octodog"
-__revision__="559790274f66fa55251f5754de34820a29c7327a"
-__build__="559790274f66fa55251f5754de34820a29c7327a"
+__version__="4.3.1"
+__copyright__="Copyright (c) 2001 - 2022 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="Sun, 24 Jul 2022 18:02:27 -0700"
+__buildsys__="M1Dog2021"
+__revision__="2cf871f2d61b489220a93d9020a0af5310054f91"
+__build__="2cf871f2d61b489220a93d9020a0af5310054f91"
 # make sure compatibility is always in place
 import SCons.compat # noqa
