3e1ef7b965cb78735b20b28667d2157a77976dad
William Deegan <bill@baddogconsulting.com>
2022-07-30 14:14:30 -0700

    updates for 4.4.0

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.4.0"
 __copyright__="Copyright (c) 2001 - 2022 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="Sat, 30 Jul 2022 14:09:41 -0700"
+__date__="Sat, 30 Jul 2022 14:11:34 -0700"
 __buildsys__="M1Dog2021"
-__revision__="4e58108dc7c67613f9115d1d2aca1d195d1e1a6c"
-__build__="4e58108dc7c67613f9115d1d2aca1d195d1e1a6c"
+__revision__="fc8d0ec215ee6cba8bc158ad40c099be0b598297"
+__build__="fc8d0ec215ee6cba8bc158ad40c099be0b598297"
 # make sure compatibility is always in place
 import SCons.compat # noqa
