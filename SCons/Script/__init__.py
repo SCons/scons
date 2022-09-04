061a76ccb6bf605022292b88ffe535c75c407d82
Anatoli Babenia <anatoli@rainforce.org>
2022-09-04 18:20:33 +0300

    Exec `EnsureSconsVersion` without init of default environment

--- SCons/Script/__init__.py
+++ SCons/Script/__init__.py
@@ -287,14 +287,17 @@ def Variables(files=None, args=ARGUMENTS):
     return SCons.Variables.Variables(files, args)
 
 
-# The list of global functions to add to the SConscript name space
-# that end up calling corresponding methods or Builders in the
+# Adding global functions to the SConscript name space.
+#
+# Static functions that do not use state in DefaultEnvironment().
+EnsureSConsVersion = _SConscript.SConsEnvironment.EnsureSConsVersion
+
+# Functions that end up calling methods or Builders in the
 # DefaultEnvironment().
 GlobalDefaultEnvironmentFunctions = [
     # Methods from the SConsEnvironment class, above.
     'Default',
     'EnsurePythonVersion',
-    'EnsureSConsVersion',
     'Exit',
     'Export',
     'GetLaunchDir',
