e6e825e7c56da38a1fe244f86baef5521e43c00a
loonycyborg <loonycyborg@gmail.com>
2022-11-22 23:13:14 +0300

    Skip gcc module test if g++ isn't found

--- test/CXX/CXXMODULES-gcc.py
+++ test/CXX/CXXMODULES-gcc.py
@@ -2,6 +2,12 @@ import TestSCons
 
 test = TestSCons.TestSCons()
 
+from SCons.Tool import gxx
+from SCons.Script import DefaultEnvironment
+
+if not gxx.exists(DefaultEnvironment()):
+    test.skip_test('g++ not found, skipping test\n')
+
 test.dir_fixture("CXX-modules-fixture")
 
 test.run(arguments = ". toolset=g++")
