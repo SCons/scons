8f3b21e9f191d7472d5ca8b401ae64b45fac959f
Mats Wichmann <mats@linux.com>
2022-11-29 09:28:00 -0700

    TestCmdTests: skip all permission fiddling on win32  [skip appveyor]
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- testing/framework/TestCmdTests.py
+++ testing/framework/TestCmdTests.py
@@ -3121,13 +3121,13 @@ class workpath_TestCase(TestCmdTestCase):
 
 
 class readable_TestCase(TestCmdTestCase):
+    @unittest.skipIf(sys.platform == 'win32', "Skip permission fiddling on win32")
     def test_readable(self):
         """Test readable()"""
         test = TestCmd.TestCmd(workdir = '', subdir = 'foo')
         test.write('file1', "Test file #1\n")
         test.write(['foo', 'file2'], "Test file #2\n")
-        if hasattr(os, 'symlink') and sys.platform != 'win32':
-            os.symlink('no_such_file', test.workpath('dangling_symlink'))
+        os.symlink('no_such_file', test.workpath('dangling_symlink'))
 
         test.readable(test.workdir, 0)
         # XXX skip these tests if euid == 0?
@@ -3160,13 +3160,13 @@ class readable_TestCase(TestCmdTestCase):
 
 
 class writable_TestCase(TestCmdTestCase):
+    @unittest.skipIf(sys.platform == 'win32', "Skip permission fiddling on win32")
     def test_writable(self):
         """Test writable()"""
         test = TestCmd.TestCmd(workdir = '', subdir = 'foo')
         test.write('file1', "Test file #1\n")
         test.write(['foo', 'file2'], "Test file #2\n")
-        if hasattr(os, 'symlink') and sys.platform != 'win32':
-            os.symlink('no_such_file', test.workpath('dangling_symlink'))
+        os.symlink('no_such_file', test.workpath('dangling_symlink'))
 
         test.writable(test.workdir, 0)
         # XXX skip these tests if euid == 0?
@@ -3195,17 +3195,14 @@ class writable_TestCase(TestCmdTestCase):
         assert not _is_writable(test.workpath('file1'))
 
 
-@unittest.skipIf(sys.platform == 'win32', "Don't run on win32")
 class executable_TestCase(TestCmdTestCase):
+    @unittest.skipIf(sys.platform == 'win32', "Skip permission fiddling on win32")
     def test_executable(self):
         """Test executable()"""
         test = TestCmd.TestCmd(workdir = '', subdir = 'foo')
         test.write('file1', "Test file #1\n")
         test.write(['foo', 'file2'], "Test file #2\n")
-
-        try: symlink = os.symlink
-        except AttributeError: pass
-        else: symlink('no_such_file', test.workpath('dangling_symlink'))
+        os.symlink('no_such_file', test.workpath('dangling_symlink'))
 
         def make_executable(fname):
             st = os.stat(fname)
