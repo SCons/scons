6d13c3bba872d41e37fc23b7abd8ea49d0caedad
William Deegan <bill@baddogconsulting.com>
2023-03-05 17:03:32 -0500

    updates

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.5.0"
 __copyright__="Copyright (c) 2001 - 2023 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="Sun, 05 Mar 2023 16:19:37 -0400"
+__date__="Sun, 05 Mar 2023 16:59:21 -0400"
 __buildsys__="M1DOG2021"
-__revision__="3d67dd35fe8fb786afc7af2bf0e31fe2a60b492b"
-__build__="3d67dd35fe8fb786afc7af2bf0e31fe2a60b492b"
+__revision__="2be4e5dc83319222cc725d13f6e98e033c042197"
+__build__="2be4e5dc83319222cc725d13f6e98e033c042197"
 # make sure compatibility is always in place
 import SCons.compat # noqa
