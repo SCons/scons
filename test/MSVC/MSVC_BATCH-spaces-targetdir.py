0904d5bfe683195ad2a5d99ccd6fbe4a0f132183
Mats Wichmann <mats@linux.com>
2023-05-27 11:54:38 -0600

    Tweak test MSVC/MSVC_BATCH-spaces-targetdir.py
    Test did not have an ending test.pass_test(), so it was not
    actually *recorded* as a pass, though it was passing. Renamed
    fixture dir.
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- test/MSVC/MSVC_BATCH-spaces-targetdir.py
+++ test/MSVC/MSVC_BATCH-spaces-targetdir.py
@@ -2,10 +2,19 @@
 #
 # Copyright The SCons Foundation
 
+"""Test that spaces in the target dir name doesn't break MSVC_BATCH."""
+
 import TestSCons
 
 test = TestSCons.TestSCons()
 
 test.skip_if_not_msvc()
-test.dir_fixture('MSVC_BATCH-spaces-targetdir')
-test.run()
+test.dir_fixture('MSVC_BATCH-spaces-fixture')
+test.run(stderr=None)  # it's enough that the build didn't fail
+test.pass_test()
+
+# Local Variables:
+# tab-width:4
+# indent-tabs-mode:nil
+# End:
+# vim: set expandtab tabstop=4 shiftwidth=4
