f55307bcd6b047ca841549362b05479abb6b5728
Mats Wichmann <mats@linux.com>
2024-02-09 13:18:42 -0700

    doc: update Pseudo description and more
    The Pseudo manpage entry is updated to be more descriptive.
    
    Since they were near neighbors in Environment.py, it and the three
    functions Precious, Repository and Requires received minor
    docstring changes there; Precious and Requires also got a minor
    tweak to the manpage entry (mainly to clarify allowable argument types
    and list return value).
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- SCons/Node/__init__.py
+++ SCons/Node/__init__.py
@@ -1230,7 +1230,7 @@ class Node(metaclass=NoSlotsPyPy):
         self.precious = precious
 
     def set_pseudo(self, pseudo: bool = True) -> None:
-        """Set the Node's precious value."""
+        """Set the Node's pseudo value."""
         self.pseudo = pseudo
 
     def set_noclean(self, noclean: int = 1) -> None:
@@ -1250,7 +1250,7 @@ class Node(metaclass=NoSlotsPyPy):
         self.always_build = always_build
 
     def exists(self) -> bool:
-        """Does this node exists?"""
+        """Reports whether node exists."""
         return _exists_map[self._func_exists](self)
 
     def rexists(self):
