20514636511ce67c8a5384bdf5767831dc6916f9
Mats Wichmann <mats@linux.com>
2024-05-10 16:36:17 -0600

    Variables cleanup: PackageVariable
    Part 3 of a series, updating the EnumVariable implementation,
    tests and docstrings.
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- SCons/Variables/PackageVariableTests.py
+++ SCons/Variables/PackageVariableTests.py
@@ -32,11 +32,11 @@ class PackageVariableTestCase(unittest.TestCase):
     def test_PackageVariable(self) -> None:
         """Test PackageVariable creation"""
         opts = SCons.Variables.Variables()
-        opts.Add(SCons.Variables.PackageVariable('test', 'test option help', '/default/path'))
+        opts.Add(SCons.Variables.PackageVariable('test', 'test build variable help', '/default/path'))
 
         o = opts.options[0]
         assert o.key == 'test', o.key
-        assert o.help == 'test option help\n    ( yes | no | /path/to/test )', repr(o.help)
+        assert o.help == 'test build variable help\n    ( yes | no | /path/to/test )', repr(o.help)
         assert o.default == '/default/path', o.default
         assert o.validator is not None, o.validator
         assert o.converter is not None, o.converter
@@ -44,7 +44,7 @@ class PackageVariableTestCase(unittest.TestCase):
     def test_converter(self) -> None:
         """Test the PackageVariable converter"""
         opts = SCons.Variables.Variables()
-        opts.Add(SCons.Variables.PackageVariable('test', 'test option help', '/default/path'))
+        opts.Add(SCons.Variables.PackageVariable('test', 'test build variable help', '/default/path'))
 
         o = opts.options[0]
 
@@ -64,11 +64,11 @@ class PackageVariableTestCase(unittest.TestCase):
 
         for t in true_values:
             x = o.converter(t)
-            assert x, "converter returned false for '%s'" % t
+            assert x, f"converter returned False for {t!r}"
 
         for f in false_values:
             x = o.converter(f)
-            assert not x, "converter returned true for '%s'" % f
+            assert not x, f"converter returned True for {f!r}"
 
         x = o.converter('/explicit/path')
         assert x == '/explicit/path', x
@@ -85,7 +85,7 @@ class PackageVariableTestCase(unittest.TestCase):
     def test_validator(self) -> None:
         """Test the PackageVariable validator"""
         opts = SCons.Variables.Variables()
-        opts.Add(SCons.Variables.PackageVariable('test', 'test option help', '/default/path'))
+        opts.Add(SCons.Variables.PackageVariable('test', 'test build variable help', '/default/path'))
 
         test = TestCmd.TestCmd(workdir='')
         test.write('exists', 'exists\n')
@@ -101,12 +101,8 @@ class PackageVariableTestCase(unittest.TestCase):
         o.validator('T', '/path', env)
         o.validator('X', exists, env)
 
-        caught = None
-        try:
+        with self.assertRaises(SCons.Errors.UserError) as cm:
             o.validator('X', does_not_exist, env)
-        except SCons.Errors.UserError:
-            caught = 1
-        assert caught, "did not catch expected UserError"
 
 
 if __name__ == "__main__":
--- test/Variables/PackageVariable.py
+++ test/Variables/PackageVariable.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -20,9 +22,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 """
 Test the PackageVariable canned Variable type.
@@ -38,8 +37,6 @@ SConstruct_path = test.workpath('SConstruct')
 def check(expect):
     result = test.stdout().split('\n')
     assert result[1:len(expect)+1] == expect, (result[1:len(expect)+1], expect)
-
-
 
 test.write(SConstruct_path, """\
 from SCons.Variables.PackageVariable import PackageVariable
@@ -75,12 +72,10 @@ test.run(arguments=['x11=%s' % test.workpath()])
 check([test.workpath()])
 
 expect_stderr = """
-scons: *** Path does not exist for option x11: /non/existing/path/
+scons: *** Path does not exist for variable 'x11': '/non/existing/path/'
 """ + test.python_file_line(SConstruct_path, 14)
 
 test.run(arguments='x11=/non/existing/path/', stderr=expect_stderr, status=2)
-
-
 
 test.pass_test()
 
