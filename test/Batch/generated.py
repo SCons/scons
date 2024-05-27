ccaffe9c9e5c7d70def83a032b8733d3fb490acf
William Deegan <bill@baddogconsulting.com>
2024-05-27 15:45:40 -0700

    cleaning up tool initialization to speed up tests

--- test/Batch/generated.py
+++ test/Batch/generated.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -20,9 +22,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 """
 Verify use of a batch builder when one of the later targets in the
@@ -34,6 +33,8 @@ import TestSCons
 test = TestSCons.TestSCons()
 
 test.write('SConstruct', """
+DefaultEnvironment(tools=[])
+
 def batch_build(target, source, env):
     for t, s in zip(target, source):
         with open(str(t), 'wb') as fp:
@@ -42,7 +43,7 @@ def batch_build(target, source, env):
                     fp.write(f.read())
             with open(str(s), 'rb') as f:
                 fp.write(f.read())
-env = Environment()
+env = Environment(tools=[])
 bb = Action(batch_build, batch_key=True)
 env['BUILDERS']['Batch'] = Builder(action=bb)
 env1 = env.Clone()
