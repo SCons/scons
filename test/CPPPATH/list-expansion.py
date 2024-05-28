0bf264eacaa8e29dec30c352f45eb2951ac3ac63
William Deegan <bill@baddogconsulting.com>
2024-05-27 21:39:37 -0700

    minimize tool initialization in tests

--- test/CPPPATH/list-expansion.py
+++ test/CPPPATH/list-expansion.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -19,10 +21,7 @@
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 
 """
 Verify that expansion of construction variables whose values are
@@ -42,6 +41,7 @@ test = TestSCons.TestSCons()
 test.subdir('sub1', 'sub2', 'sub3', 'sub4')
 
 test.write('SConstruct', """\
+DefaultEnvironment(tools=[])
 class _inc_test:
     def __init__(self, name):
         self.name = name
