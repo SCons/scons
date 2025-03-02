bc527d0626289e52bd8d214b9077a9f0b55cc982
William Deegan <bill@baddogconsulting.com>
2025-03-02 14:37:53 -0800

    fix copyright and other dates in users guide

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
 __version__="4.9.0"
 __copyright__="Copyright (c) 2001 - 2025 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="Sun, 02 Mar 2025 14:04:50 -0700"
+__date__="Sun, 02 Mar 2025 14:33:49 -0700"
 __buildsys__="M1Dog2021"
-__revision__="99a8c86de1ce91d23b102520e185c54ebd968924"
-__build__="99a8c86de1ce91d23b102520e185c54ebd968924"
+__revision__="f6f2fd98b2a94871b506e517873af4d6faf04ed9"
+__build__="f6f2fd98b2a94871b506e517873af4d6faf04ed9"
 # make sure compatibility is always in place
 import SCons.compat  # noqa
