fd2015cd0f9628c37c41599d4703fb207922eb99
William Deegan <bill@baddogconsulting.com>
2025-03-02 14:21:45 -0800

    post release

--- testing/framework/TestSCons.py
+++ testing/framework/TestSCons.py
@@ -54,7 +54,7 @@ from TestCommon import __all__, _python_
 # here provides some independent verification that what we packaged
 # conforms to what we expect.
 
-default_version = '4.8.2ayyyymmdd'
+default_version = '4.9.1'
 
 # TODO: these need to be hand-edited when there are changes
 python_version_unsupported = (3, 7, 0)
