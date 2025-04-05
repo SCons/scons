54b843ab7d3e9d6e3e4b429b87d4e0d44f05f962
William Deegan <bill@baddogconsulting.com>
2025-04-05 12:53:17 -0700

    updated tests to use f-string and skip str()'ing node

--- test/implicit/changed-node.py
+++ test/implicit/changed-node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
-# __COPYRIGHT__
+# MIT License
+#
+# Copyright The SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -20,9 +22,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
-__revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 """
 Verify that we don't throw an exception if a stored implicit
@@ -51,7 +50,7 @@ def lister(target, source, env):
             for l in os.listdir(source[0]):
                 ofp.write(l + '\\n')
         else:
-            ofp.write(str(source[0]) + '\\n')
+            ofp.write(f'{source[0]}\\n')
 
 builder = Builder(action=lister,
                   source_factory=Dir,
@@ -76,6 +75,7 @@ test.run('--debug=stacktrace')
 
 
 test.write('SConstruct', """\
+DefaultEnvironment(tools=[])
 SetOption('implicit_cache', 1)
 SetOption('max_drift', 1)
 
@@ -86,7 +86,7 @@ def lister(target, source, env):
             for l in os.listdir(source[0]):
                 ofp.write(l + '\\n')
         else:
-            ofp.write(str(source[0]) + '\\n')
+            ofp.write(f'{source[0]}\\n')
 
 builder = Builder(action=lister,
                   source_factory=File)
