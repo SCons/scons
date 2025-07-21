0e29e4fc968bfe6086841dd754956652b9060d47
William Deegan <bill@baddogconsulting.com>
2025-07-20 20:49:06 -0700

    Fix Issue #4746 by changing encoding of tempfile's to use locale.getpreferredencoding(False) instead of hardcoded utf-8

--- SCons/Platform/__init__.py
+++ SCons/Platform/__init__.py
@@ -47,6 +47,7 @@ import importlib
 import os
 import sys
 import tempfile
+import locale
 
 import SCons.Errors
 import SCons.Subst
@@ -279,7 +280,7 @@ class TempFileMunge:
         tempfile_esc_func = env.get('TEMPFILEARGESCFUNC', SCons.Subst.quote_spaces)
         args = [tempfile_esc_func(arg) for arg in cmd[1:]]
         join_char = env.get('TEMPFILEARGJOIN', ' ')
-        os.write(fd, bytearray(join_char.join(args) + "\n", encoding="utf-8"))
+        os.write(fd, bytearray(join_char.join(args) + "\n", encoding=locale.getpreferredencoding(False)))
         os.close(fd)
 
         # XXX Using the SCons.Action.print_actions value directly
