7e1b28f44c70116b78abaa011ede5dcbc65ad8d1
William Deegan <bill@baddogconsulting.com>
2025-10-02 10:23:01 -0700

    Release build of 4.10.0

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
-__version__="4.9.1"
+__version__="4.10.0"
 __copyright__="Copyright (c) 2001 - 2025 The SCons Foundation"
 __developer__="bdbaddog"
-__date__="Thu, 27 Mar 2025 11:44:24 -0700"
+__date__="Thu, 02 Oct 2025 10:16:02 -0700"
 __buildsys__="M1Dog2021"
-__revision__="39a12f34d532ab2493e78a7b73aeab2250852790"
-__build__="39a12f34d532ab2493e78a7b73aeab2250852790"
+__revision__="b3e979a3e42c43b91f7624895910afb869a19806"
+__build__="b3e979a3e42c43b91f7624895910afb869a19806"
 # make sure compatibility is always in place
 import SCons.compat  # noqa
