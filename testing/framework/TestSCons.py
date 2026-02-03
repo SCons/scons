10fafe3702f67fd6e134795b442a91e14498430b
Thaddeus Crews <repiteo@outlook.com>
2026-02-03 12:44:25 -0600

    Deprecate Python 3.8

--- testing/framework/TestSCons.py
+++ testing/framework/TestSCons.py
@@ -58,8 +58,8 @@ default_version = '4.10.2ayyyymmdd'
 
 # TODO: these need to be hand-edited when there are changes
 python_version_unsupported = (3, 8, 0)
-python_version_deprecated = (3, 8, 0)
-python_version_supported_str = "3.8.0"  # str of lowest non-deprecated Python
+python_version_deprecated = (3, 9, 0)
+python_version_supported_str = "3.9.0"  # str of lowest non-deprecated Python
 
 SConsVersion = default_version
 
