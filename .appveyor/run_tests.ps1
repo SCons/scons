d8e1c1f691c64e804fdbc08e3857018195c86f57
William Deegan <bill@baddogconsulting.com>
2026-04-01 21:52:11 -0700

    [skip actions] limit tests to simplify testing

--- .appveyor/run_tests.ps1
+++ .appveyor/run_tests.ps1
@@ -1,10 +1,10 @@
 if ($env:COVERAGE -eq 1) {
     & "$env:SCONS_PYTHON_BIN" -m coverage run -p --rcfile "$env:COVERAGE_PROCESS_START" runtest.py -j 2 -t --exclude-list exclude_list.txt -a
 } else {
-    & "$env:SCONS_PYTHON_BIN" runtest.py -j 2 -t --exclude-list exclude_list.txt -a
+    & "$env:SCONS_PYTHON_BIN" runtest.py -j 2 -t --exclude-list exclude_list.txt test/Actions
 }
 
-# Treat exit code 2 (some tests failed) as success for AppVeyor 
+# Treat exit code 2 (some tests failed) as success for AppVeyor
 # as per original configuration.
 if ($LastExitCode -eq 2 -or $LastExitCode -eq 0) {
     exit 0
