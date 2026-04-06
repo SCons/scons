b09f94ab3bdd49662e69400fcf8f927a00494ba2
William Deegan <bill@baddogconsulting.com>
2026-04-06 13:37:53 -0700

    [skip actions] Re-enable full testing on appveyor

--- .appveyor/run_tests.ps1
+++ .appveyor/run_tests.ps1
@@ -5,10 +5,10 @@ Write-Host "SCONS_PYTHON_BIN: $env:SCONS_PYTHON_BIN"
 if ($env:COVERAGE -eq 1) {
     & "$env:SCONS_PYTHON_BIN" -m coverage run -p --rcfile "$env:COVERAGE_PROCESS_START" runtest.py -j 2 -t --exclude-list exclude_list.txt -a
 } else {
-    & "$env:SCONS_PYTHON_BIN" runtest.py -j 2 -t --exclude-list exclude_list.txt test/Actions
+    & "$env:SCONS_PYTHON_BIN" runtest.py -j 2 -t --exclude-list exclude_list.txt
 }
 
-# Treat exit code 2 (some tests failed) as success for AppVeyor 
+# Treat exit code 2 (some tests failed) as success for AppVeyor
 # as per original configuration.
 if ($LastExitCode -eq 2 -or $LastExitCode -eq 0) {
     Write-Host "Exiting .appveyor/run_tests.ps1"
