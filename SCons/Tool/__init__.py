ec30132538911a3b5cc79eab6324a7677ea98a04
Mats Wichmann <mats@linux.com>
2026-04-12 06:34:02 -0600

    Minor tweaks in tool-init
    just an import and a precautionary "str" conversion
    
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- SCons/Tool/__init__.py
+++ SCons/Tool/__init__.py
@@ -55,6 +55,7 @@ import sys
 import os
 import importlib.util
 
+import SCons.Action
 import SCons.Builder
 import SCons.Errors
 import SCons.Node.FS
@@ -288,7 +289,7 @@ class Tool:
                 kw.update(call_kw)
             else:
                 kw = self.init_kw
-        env.AppendUnique(TOOLS=[self.name])
+        env.AppendUnique(TOOLS=[str(self.name)])
         if hasattr(self, 'options'):
             import SCons.Variables
             if 'options' not in env:
